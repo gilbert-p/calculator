b5df42a496994215a7744c9734a64a5471dc1e2f
Gilberto Placidon <gilbertoplacidon@gmail.com>
2019-11-27 12:03:40 -0800

    Posfix function can now handle parentheses
    Can now handle basic equations containing parentheses.
    Definition of basic example: "4 + 18 / ( 9 − 3 )"
    Types of equations where issue is occurring: "(3 x 4 / ( 2 + 5 ) )"

--- shunting-yard.js
+++ shunting-yard.js
@@ -2,7 +2,9 @@
 //Example output "4,18,9,3,−,/,+"
 
 //Input as represented in Token Array
-let tokenArray = ["4", "+", "3", "x", "9"];
+let tokenArray_2 = ["(","5", "+", "3", ")", "x", "3"];
+let tokenArray = ["(","3","x","4","/","(","2", "+","5",")",")"];
+let tokenArray_3 = ["4", "+", "18", "/", "(", "9", "-", "3", ")"];
 
 // let tokenArray = ["4", "x", "18", "+", "9", "-", "3"];
 
@@ -12,13 +14,13 @@ let operators = [];
 let outputQueue = [];
 
 let precedenceArray = [];
-precedenceArray["+"] = 0;
-precedenceArray["-"] = 0;
-precedenceArray["x"] = 1;
-precedenceArray["/"] = 1;
-precedenceArray["%"] = 1;
-precedenceArray["("] = 0;
-precedenceArray[")"] = 0;
+precedenceArray["+"] = 2;
+precedenceArray["-"] = 2;
+precedenceArray["x"] = 3;
+precedenceArray["/"] = 3;
+precedenceArray["%"] = 3;
+precedenceArray["("] = 1;
+precedenceArray[")"] = -1;
 
 const checkIfNumber = input => {
     let input_num = parseInt(input);
@@ -36,25 +38,6 @@ const getPrecedence = (operator) => {
 }
 
 
-
-// const separateTokens = () => {
-//     let temp_tokenArray = [...tokenArray];
-
-//     //while there are tokenArray has elements
-//     while (temp_tokenArray.length > 0) {
-
-//         let currentToken = temp_tokenArray.shift();
-
-//         if (checkIfNumber(currentToken)) {
-//             outputQueue.push(currentToken);
-//         } else {
-//             operators.push(currentToken);
-//         }
-//     }
-// }
-
-
-
 const postFix = () => {
     let result = "";
     //output queue does not contain the parentheses operators
@@ -64,7 +47,7 @@ const postFix = () => {
 
     let test_loop = 0;
 
-    // while(test_loop < 3){
+    // while(test_loop < 10){
     while (temp_tokenArray.length > 0) {
 
 
@@ -75,46 +58,67 @@ const postFix = () => {
             console.log(`1st Input: ${outputQueue}`);
         } else {
 
-            console.log(`Current operator: ${currentToken}`);
 
-            console.log("current token: " + getPrecedence(currentToken));
-            if (operators.length > 0) {
 
-                if (getPrecedence(operators[operators.length - 1]) > getPrecedence(currentToken)) {
+            if(currentToken == "("){
+                operators.push(currentToken);
+            }
+            else if(currentToken == ")"){
+
+                //Edge case: check whether or not the user inputted a left bracket in the first place.
+                while(operators[operators.length -1] != "("){
                     let op = operators.pop();
                     outputQueue.push(op);
+                }
+                //pop left bracket
+                operators.pop();
 
-                    //Add token back to beginning of token array
-                    temp_tokenArray.unshift(currentToken);
+            }
+            else {
+
+                if (operators.length > 0) {
+
+                        if (getPrecedence(operators[operators.length - 1]) > getPrecedence(currentToken)) {
+                            let op = operators.pop();
+                            outputQueue.push(op);
+        
+                            //Add token back to beginning of token array
+                            temp_tokenArray.unshift(currentToken);
+
+                        } else {
+                            operators.push(currentToken);
+                        }
+
+ 
                 } else {
                     operators.push(currentToken);
                 }
-                console.log("stack tail: " + getPrecedence(operators[operators.length - 1]));
-            } else {
-                operators.push(currentToken);
             }
 
-
-            // operators.push(currentToken);
-
-
+            console.log("outputQueue:");
+            console.log(outputQueue);
+            console.log("operators:");
+            console.log(operators);
+            console.log("\n\n\n");
 
         }
-
         test_loop++;
     }
 
 
+    //pop off any remaining operators after iterating token array
     while (operators.length > 0) {
         outputQueue.push(operators.pop());
     }
 
-    console.log("TokenArray: ", temp_tokenArray);
-    console.log("OperatorStack: ", operators);
-    console.log("OutputQueue: ", outputQueue);
+    console.log("Final Output: ");
+    console.log(outputQueue);
 
-    console.log("\n\n\n")
-    loop_iterations++;
+    // console.log("TokenArray: ", temp_tokenArray);
+    // console.log("OperatorStack: ", operators);
+    // console.log("OutputQueue: ", outputQueue);
+
+    // console.log("\n\n\n");
 }
 
 
