33e615bdfbcc39e57dc0c6f7b14608d16f82bb2c
epoxi117 <gplacid2@calstatela.edu>
2019-11-30 20:41:16 -0800

    Todo: Need to add modulus functionality

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
+// const calculate_input = require('./shunting-yard');
+
 let tokenArray = [];
 let currentNumber = "";
+let result = 0;
 
 window.addEventListener("DOMContentLoaded", function () {
     let calculatorButtons = document.getElementsByClassName("calcBtn");
@@ -21,8 +24,14 @@ window.addEventListener("DOMContentLoaded", function () {
         return false;
     };
 
+    const input_exists = (input) => {
+        return (input.length > 0) ? true : false;
+    }
+
+
     const printToView = () => {
         let viewText = "";
+
 
         if (currentNumber.length > 0) {
 
@@ -31,21 +40,19 @@ window.addEventListener("DOMContentLoaded", function () {
                     viewText += token + " ";
                 });
                 viewText += currentNumber;
-                document.getElementById("view").innerHTML = viewText;
+                document.getElementById("txt-view").innerHTML = viewText;
             } else {
-                document.getElementById("view").innerHTML = currentNumber;
+                document.getElementById("txt-view").innerHTML = currentNumber;
             }
 
         } else {
             tokenArray.forEach(token => {
                 viewText += token + " ";
             });
-            document.getElementById("view").innerHTML = viewText;
+            document.getElementById("txt-view").innerHTML = viewText;
         }
 
-
-
-
+        console.log("printing");
         console.log(tokenArray);
     };
 
@@ -63,45 +70,96 @@ window.addEventListener("DOMContentLoaded", function () {
             console.log("true");
             currentNumber += btnInput;
             printToView();
+
         } else {
+            if (btnInput == "dot") {
+                // result = 0;
+                if (input_exists(currentNumber)) {
+                    // tokenArray.push(currentNumber);
+                    // tokenArray.push("+");
+                    currentNumber += ".";
+                } else {
+                    currentNumber = "";
+                }
+            }
+
             if (btnInput == "add") {
-                tokenArray.push(currentNumber);
-                tokenArray.push("+");
+                result = 0;
+                if (input_exists(currentNumber)) {
+                    tokenArray.push(currentNumber);
+                    tokenArray.push("+");
+                } else {
+                    tokenArray.push("+");
+                }
+
                 currentNumber = "";
             }
 
             if (btnInput == "subtract") {
-                tokenArray.push("-");
+                result = 0;
+                if (input_exists(currentNumber)) {
+                    tokenArray.push(currentNumber);
+                    tokenArray.push("-");
+                } else {
+                    tokenArray.push("-");
+                }
+
                 currentNumber = "";
             }
 
             if (btnInput == "multiply") {
+                result = 0;
+                if (input_exists(currentNumber)) {
+                    tokenArray.push(currentNumber);
+                    tokenArray.push("x");
+                } else {
+                    tokenArray.push("x");
+                }
 
-                tokenArray.push("x");
                 currentNumber = "";
             }
 
             if (btnInput == "division") {
-                tokenArray.push("/");
+                result = 0;
+                if (input_exists(currentNumber)) {
+                    tokenArray.push(currentNumber);
+                    tokenArray.push("/");
+                } else {
+                    tokenArray.push("/");
+                }
+
                 currentNumber = "";
             }
 
             if (btnInput == "left-parentheses") {
+
                 tokenArray.push("(");
-                currentNumber = "";
             }
 
             if (btnInput == "right-parentheses") {
+                if (checkIfNumber(currentNumber)) {
+                    tokenArray.push(currentNumber);
+                    currentNumber = "";
+                }
+
                 tokenArray.push(")");
             }
 
             if (btnInput == "equals") {
+                result = 0;
+                console.log("equals");
+                tokenArray.push(currentNumber);
+                result = postFix(tokenArray);
+                tokenArray = [];
+                tokenArray.push(result);
+                result = 0;
                 currentNumber = "";
             }
 
             if (btnInput == "clear") {
                 tokenArray = [];
                 currentNumber = "";
+                result = 0;
             }
         }
         printToView();
--- shunting-yard.js
+++ shunting-yard.js
@@ -1,10 +1,7 @@
 //Example input "4+18/(9-3)"
 //Example output "4,18,9,3,−,/,+"
 
-//Input as represented in Token Array
-let tokenArray_2 = ["(","5", "+", "3", ")", "x", "3"];
-let tokenArray = ["(","3","x","4","/","(","2", "+","5",")",")"];
-let tokenArray_3 = ["4", "+", "18", "/", "(", "9", "-", "3", ")"];
+
 
 // let tokenArray = ["4", "x", "18", "+", "9", "-", "3"];
 
@@ -38,12 +35,10 @@ const getPrecedence = (operator) => {
 }
 
 
-const postFix = () => {
-    let result = "";
+const postFix = (tokenArray) => {
     //output queue does not contain the parentheses operators
-    let output_length = tokenArray.length;
+
     let temp_tokenArray = [...tokenArray];
-    let loop_iterations = 0;
 
     let test_loop = 0;
 
@@ -55,52 +50,51 @@ const postFix = () => {
 
         if (checkIfNumber(currentToken)) {
             outputQueue.push(currentToken);
-            console.log(`1st Input: ${outputQueue}`);
         } else {
 
 
 
-            if(currentToken == "("){
+            if (currentToken == "(") {
                 operators.push(currentToken);
-            }
-            else if(currentToken == ")"){
+            } else if (currentToken == ")") {
 
                 //Edge case: check whether or not the user inputted a left bracket in the first place.
-                while(operators[operators.length -1] != "("){
+                while (operators[operators.length - 1] != "(") {
                     let op = operators.pop();
                     outputQueue.push(op);
                 }
                 //pop left bracket
                 operators.pop();
 
-            }
-            else {
+            } else {
 
                 if (operators.length > 0) {
 
-                        if (getPrecedence(operators[operators.length - 1]) > getPrecedence(currentToken)) {
-                            let op = operators.pop();
-                            outputQueue.push(op);
-        
-                            //Add token back to beginning of token array
-                            temp_tokenArray.unshift(currentToken);
+                    if (getPrecedence(operators[operators.length - 1]) > getPrecedence(currentToken)) {
+                        let op = operators.pop();
+                        outputQueue.push(op);
 
-                        } else {
-                            operators.push(currentToken);
-                        }
+                        //Add token back to beginning of token array
+                        temp_tokenArray.unshift(currentToken);
 
- 
+                    } else if (getPrecedence(operators[operators.length - 1]) == getPrecedence(currentToken)) {
+                        let op = operators.pop();
+                        outputQueue.push(op);
+
+                        //Add token back to beginning of token array
+                        temp_tokenArray.unshift(currentToken);
+                    } else {
+                        console.log("greater precedence: " + currentToken);
+                        operators.push(currentToken);
+                        console.log(operators);
+                    }
+
+
                 } else {
+                    console.log(" push reg token: " + currentToken);
                     operators.push(currentToken);
                 }
             }
-
-            console.log("outputQueue:");
-            console.log(outputQueue);
-            console.log("operators:");
-            console.log(operators);
-            console.log("\n\n\n");
-
         }
         test_loop++;
     }
@@ -111,18 +105,80 @@ const postFix = () => {
         outputQueue.push(operators.pop());
     }
 
-    console.log("Final Output: ");
-    console.log(outputQueue);
+    let ans = calculate(outputQueue);
+    return ans;
 
-    // console.log("TokenArray: ", temp_tokenArray);
-    // console.log("OperatorStack: ", operators);
-    // console.log("OutputQueue: ", outputQueue);
+}
 
-    // console.log("\n\n\n");
+const calculate = (post_Fix_list) => {
+
+    let number_stack = [];
+
+    for (let ii = 0; ii < post_Fix_list.length; ii++) {
+
+
+        if (checkIfNumber(post_Fix_list[ii])) {
+            number_stack.push(post_Fix_list[ii]);
+        } else if (post_Fix_list[ii].length > 0) {
+            //It's an operator
+
+            let op = post_Fix_list[ii];
+            let first_input = parseFloat(number_stack.pop());
+            let second_input = parseFloat(number_stack.pop());
+            let result = 0;
+
+            switch (op) {
+                case "+":
+                    result = first_input + second_input;
+                    number_stack.push(result);
+                    console.log("add: " + result);
+                    result = 0;
+                    break;
+                case "-":
+                    result = second_input - first_input;
+                    number_stack.push(result);
+                    console.log("subtract: " + result);
+                    result = 0;
+                    break;
+                case "x":
+                    result = first_input * second_input;
+                    console.log("multiply before push: " + result);
+                    number_stack.push(result);
+                    console.log("multiply after push: " + result);
+                    console.log(number_stack);
+                    result = 0;
+                    break;
+                case "/":
+                    result = second_input / first_input;
+                    //If user divided by Zero
+                    if (!isFinite(result)) {
+                        console.log("You tried to divide by 0");
+                        result.push("Infinity");
+                        break;
+                    }
+                    number_stack.push(result);
+                    console.log("divide: " + result);
+                    result = 0;
+                    break;
+            }
+        }
+    }
+
+    //Clear for next input
+    outputQueue = [];
+    operators = [];
+    console.log("outer stack: ");
+    console.log(number_stack);
+    console.log("answer returned: ", number_stack[0]);
+    return number_stack[0];
 }
 
 
+//Input as represented in Token Array
+let test_array_1 = ["(", "3", "x", "4", "/", "(", "2", "+", "5", ")", ")"];
+let test_array_2 = ["(", "5", "+", "3", ")", "x", "3"];
+let tokenArray_3 = ["4", "+", "18", "/", "(", "9", "-", "3", ")"];
+let tokenArray_4 = ["4", "/", "2", "x", "3", "+", "1"];
+let tokenArray_5 = ["(", "89", "+", "6", ")"];
 
-
-postFix();
-// console.log(outputQueue);
+// postFix(tokenArray_5);
